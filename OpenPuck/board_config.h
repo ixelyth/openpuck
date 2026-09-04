0d1b378e9e75c52b6ea119e685257f4c3c1ac17e
Edgar <edgar.d.hdez@gmail.com>
2026-09-04 14:30:22 -0400

    Add Makerdiary MDK USB Dongle support
    Co-authored-by: stickman-dev <50963269+stickman-dev@users.noreply.github.com>

--- OpenPuck/board_config.h
+++ OpenPuck/board_config.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#if defined(OPK_BOARD_MDBT50Q_CX_40)
+#if defined(OPK_BOARD_MDK_USB_DONGLE)
+// Makerdiary/GeeekPi MDK USB Dongle uses the Adafruit-format
+// settings page and S140 6.1.1 layout expected by OpenPuck WebUSB.
+#define OPK_HAS_ADAFRUIT_DFU 1
+#elif defined(OPK_BOARD_MDBT50Q_CX_40)
 // The staged updater rewrites an Adafruit-format bootloader settings page.
 #define OPK_HAS_ADAFRUIT_DFU 0
 #else
