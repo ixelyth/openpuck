9a956818e5c38dbdb2428f92512fda3f08ff9532
Edgar <edgar.d.hdez@gmail.com>
2026-09-06 12:05:40 -0400

    Expose exact M29 validation failure

--- tools/f27_m29_ci_v3.sh
+++ tools/f27_m29_ci_v3.sh
@@ -5,4 +5,4 @@ tmp="$(mktemp)"
 trap 'rm -f "$tmp"' EXIT
 sed 's#python3 tools/f27_m29_pair_roles.py --session0#python3 tools/f27_m29_pair_roles_v3.py --session0#' \
   tools/f27_m29_ci.sh > "$tmp"
-bash "$tmp" "$@"
+bash -x "$tmp" "$@"
